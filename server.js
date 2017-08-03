4e2adc34151570b0300753cd009744130f36d5ff
thiru321 <30630726+thiru321@users.noreply.github.com>
2017-08-03 21:51:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/Article-one', function (req,res){
+    res.send('Article one will be requested and served here');
+});
+app.get('/Article-two', function (req,res){
+    res.send('Article two will be requested and served here');
+});
+app.get('/Article-three', function (req,res){
+    res.send('Article three will be requested and served here');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });

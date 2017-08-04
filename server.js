69180fb0ac62b23f82ce36fd59f86ee237677820
thiru321 <30630726+thiru321@users.noreply.github.com>
2017-08-04 12:27:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,52 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne={
+    heading: 'Article 1',
+    date: 'sep 5,2016',
+    content: `<p>
+                This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+            </p>
+             <p>
+                This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+            </p>
+            <p>
+                This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+            </p>'
+};
+
+
+var htmlTemplate = `
+<html>
+    <head>
+        <title>
+            Article 1 | thiru
+            <meta name="Viewport" content="width=device-width, initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container"> />
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+            S{heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+           ${content}
+        </div>
+        </div>
+        </body>
+        </html>
+
+
+
+`;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });

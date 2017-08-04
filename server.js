3c66c281d9bdd5f7afb9c31344affcdf9d5d18c5
thiru321 <30630726+thiru321@users.noreply.github.com>
2017-08-04 20:42:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,51 +6,6 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var articleOne={
-    heading: 'Article 1',
-    date: 'sep 5,2016',
-    content: `<p>
-                This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-            </p>
-             <p>
-                This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-            </p>
-            <p>
-                This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-            </p>'
-};
-
-
-var htmlTemplate = `
-<html>
-    <head>
-        <title>
-            Article 1 | thiru
-            <meta name="Viewport" content="width=device-width, initial-scale=1" />
-        <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container"> />
-        <div>
-            <a href="/">Home</a>
-        </div>
-        <hr/>
-        <h3>
-            S{heading}
-        </h3>
-        <div>
-            se5 5, 2016
-        </div>
-        <div>
-            This is the content for my first article.
-        </div>
-        </div>
-    </body>
-</html>
-
-
-
-`;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -75,7 +30,7 @@ app.get('/ui/madi.png', function (req, res) {
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 80;
+var port = 8080;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });

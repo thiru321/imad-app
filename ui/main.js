3c0d17e1009a8c87068cdd2980848fd8226e90a3
thiru321 <30630726+thiru321@users.noreply.github.com>
2017-08-06 15:14:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,14 +30,30 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
+    // create a request object.
+     var request = new  XMLHttpRequest();
+      // captue the response and store it in a variable.
+    request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.DONE) {
+          // take some action
+          if(request.status === 200){
+              //capture the list of names and render it as a list.
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list='';
+                for (var i=0; i< names.length; i++){
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+             
+     
+          }
+      }
+    };
     //make a request to the server and send the name.
     
-    //capture the list of names and render it as a list.
-    var names=['name1','name2','name3','name4'];
-    var list='';
-    for (var i=0; i< names.length; i++){
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+ // make the request
+    request.open('GET','http://kdinacse.imad.hasura-app.io/sumbit-name?name=' + name,true);
+    request.send(null);
 };

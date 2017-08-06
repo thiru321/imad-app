ae560580fe18b0d2ee993bc175fc9739ee0850d0
thiru321 <30630726+thiru321@users.noreply.github.com>
2017-08-06 14:49:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get('/counter', function (req,res){
     counter = counter + 1;
     res.send(counter.toString());
 });
-app.get('/:Article-One', function (req,res){
+app.get('/:articleName', function (req,res){
     res.send(createTemplate(ArticleOne));
 });
 app.get('/Article-two', function (req,res){

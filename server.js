aa08fa86539cd46de7e9a8cdade455d5a456fe78
thiru321 <30630726+thiru321@users.noreply.github.com>
2017-08-22 15:40:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').pool;
 
-var config = {
-    user: '	kdinacse',
-    database: '	kdinacse',
-    host: 'db.imad.hasura-app.io',
-    port: '8080',
-    password: process.env.DB_PASSWORD
-}
 var app = express();
 app.use(morgan('combined'));
 

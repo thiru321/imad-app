dcf8d62329ea159ddced5fe3299a2eb9f766a464
thiru321 <30630726+thiru321@users.noreply.github.com>
2017-08-24 15:47:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config = {
     user: '	kdinacse',
     database: '	kdinacse',
     host: 'db.imad.hasura-app.io',
-    port: '8080',
+    port: '5432',
     password: process.env.DB_PASSWORD
 }
 var app = express();

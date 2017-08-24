993ee25b6651430769c0421fbcb241d23ac69f9b
thiru321 <30630726+thiru321@users.noreply.github.com>
2017-08-24 15:55:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ function createTemplate (data) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var Pool = new Pool(config);
+var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
     // make a select request
     // return a response with the results
